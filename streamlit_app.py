560f3de3bd71455a983e6a81080ea553295a4a9f
Dani Oria <56119480+0ria@users.noreply.github.com>
2023-01-18 12:35:48 +0100

    Update streamlit_app.py
    Erased line that diplays json text

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,7 @@ streamlit.dataframe(fruits_to_show)
 #New Cestion to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) # just writes the data to screen
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # normalize the data
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
